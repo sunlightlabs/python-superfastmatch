614231d1dd4629a66820d8bb8ea3865f95757182
Drew Vogel <dvogel@sunlightfoundation.com>
2012-03-22 17:55:42 -0400

    Added bare bones setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-superfastmatch',
+      version='0.1',
+      description='Python client API for Superfastmatch',
+      author='Drew Vogel',
+      author_email='dvogel@sunlightfoundation.com',
+      packages=['superfastmatch']
+     )
